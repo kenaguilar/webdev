d56b5d6bbc4c15c58fb83e338d87a196387a79ec
KenethAguilar <70752738+KenethAguilar@users.noreply.github.com>
2020-09-11 16:52:00 +0800

    Add file

---
+++ js.js
@@ -0,0 +1,19 @@
+<script>
+	$(document).ready(function(){
+		var x = 0;
+    // for next slide
+		$('.btn-next').click(function(){
+    
+        x= (x<=300)?(x+100):0;
+      $('figure').css('left', -x+'%');
+		});
+
+
+     // for prev slide
+     $('.btn-prev').click(function(){
+    
+        x= (x>=100)?(x-100):400;
+      $('figure').css('left', -x+'%');
+    });
+	});
+</script>
